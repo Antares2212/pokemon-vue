eafa12d2c7b27805c66974bad575bb3b2ea2bc5d
alex_shatailov <Alakey812@yandex.ru>
2023-01-21 14:35:01 +0300

    add ui kit

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,61 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&family=Manrope:wght@700&family=Poppins:wght@300;400;500&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+  border: 0;
+  background: transparent;
+}
+
+body {
+  width: 100%;
+  min-height: 100%;
+  margin: 0;
+  padding: 0;
+  background: #f4f4f4;
+  font: 14px 'Manrope', sans-serif;
+  font-weight: 400;
+  line-height: normal;
+  overflow-x: hidden;
+  letter-spacing: 0.02em;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 95%;
+  min-width: 320px;
+  margin: 0 auto;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+a, .btn {
+  color: inherit;
+  text-decoration: none;
+  transition: all .3s;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+input[type="text"], input[type="password"], select, textarea {
+  width: 100%;
+  background-color: #fff;
+  color: #000;
+  box-shadow: inset 0 0 0 1px #e3e3e3, inset 1px 2px 5px rgb(0 0 0 / 10%);
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+input[type="text"], input[type="password"] {
+  height: 40px;
+  line-height: 40px;
+  border-radius: 4px;
+  padding: 0 15px;
+}
+
+ul {
+  list-style: none;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    
+    <app-button>
+      vcxvcxf
+    </app-button>
+    <app-input/>
+    <app-select/>
   </div>
 </template>
 
