98172dd958250a5b6f7939072b02a33437c3661e
alex_shatailov <Alakey812@yandex.ru>
2023-01-26 00:32:41 +0300

    add card page

--- src/main.js
+++ src/main.js
@@ -6,17 +6,24 @@ import components from '@/components/UI'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { dom } from "@fortawesome/fontawesome-svg-core";
 
 const app = createApp(App) 
 
 const icons = [
-    faDiscord
+    fas,
+    fab,
+    far
 ]
 
 icons.forEach(icon => {
     library.add(icon)
 });
+
+dom.watch();
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 
