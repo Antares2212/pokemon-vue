26ff173ac4c9c96b9898bd07d541bad1b013f653
alex_shatailov <Alakey812@yandex.ru>
2023-01-27 11:49:45 +0300

    add help page

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -2,8 +2,10 @@ import AppButton from '@/components/UI/AppButton'
 import AppInput from '@/components/UI/AppInput'
 import AppDialog from '@/components/UI/AppDialog'
 import AppSelect from '@/components/UI/AppSelect'
+import AppAcordion from '@/components/UI/AppAcordion'
 
 export default [
+    AppAcordion,
     AppButton,
     AppInput,
     AppDialog,
